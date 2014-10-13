eda9fba72e32d4090786cf8a954b00cf15604319
suprbh <supb27@yahoo.com>
2014-10-13 15:44:14 -0700

    displays the dragged and dropped image and lists its name, size.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -20,9 +20,33 @@ app = {
 		var files = evnt.dataTransfer.files;
 
 		var output = [];
+		var render = [];
 		for (var i = 0; i < files.length; i++){
 			var f = files[i];
+			if (!f.type.match('image.*') && !f.type.match('video.*') && !f.type.match('audio.*')){
+				alert(f.name + " is not a media file (image/audio/video) !");
+				continue;
+			}
+
+
 			output.push('<li>', escape(f.name), '(', f.type || 'n/a', ') - ', f.size, 'bytes</li>');
+
+			var reader = new FileReader();
+			reader.onload = (function(theFile) {
+				// console.log(reader.result);
+				return function(e) {
+					var render = [];
+					// render the image onto the same div box
+					if (f.type.match('image.*')){
+            var span = document.createElement('span');
+						span.innerHTML = ['<img class="thumb" src="', e.target.result, '" title="', escape(theFile.name),'"/>'].join('');
+            // console.log(span);
+            document.getElementById('drop').appendChild(span);
+					}
+				}
+			})(f);
+
+			reader.readAsDataURL(f);
 		}
 		document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
 	},
